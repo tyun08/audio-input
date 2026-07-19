47d26a682efc2bb151f31fed753e4e2e1f1d1558
Tony Yun <jtcloud123+app@gmail.com>
2026-07-19 11:41:28 -0400

    fix: relaunch app after installing update

--- src-tauri/src/updates.rs
+++ src-tauri/src/updates.rs
@@ -87,8 +87,7 @@ pub async fn install_update(
     update
         .download_and_install(|_, _| {}, || {})
         .await
-        .map_err(|e| e.to_string())?;
-    app.restart();
+        .map_err(|e| e.to_string())
 }
 
 #[cfg(test)]
